0eac7188838c59e028e0269838c4cd820006f6c9
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-04-16 19:30:52 -0400

    creating constructor function for blog posts and creating print meta data code

--- 02-Stu_Constructors/Unsolved/index.js
+++ 02-Stu_Constructors/Unsolved/index.js
@@ -1,7 +1,21 @@
 // TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title', 'text, and 'createdOn'
+function BlogPost(authorName, title, text, createdOn) {
+  this.authorName = authorName;
+  this.title = title;
+  this.text = text;
+  this.createdOn = createdOn;
+  this.printMetaData = function() {
+    console.log(`Created by ${this.authorName} on ${this.createdOn}`);
+  };
+}
 
 // TODO: Include a method called 'printMetaData()' that prints a message in the console saying 'Created by (authorName) on (createdOn)'
 
 // TODO: Create a new object using the 'BlogPost' constructor
+const blogPost = new BlogPost('John Doe', 'My First Post', 'This is my first post', '01/01/2020');
+const blogPost1 = new BlogPost('Lana Rey', 'I Have a Dream', 'This is my dream', '11/25/2023');
 
 // TODO: Call the 'printMetaData()' method on the new object
+blogPost.printMetaData();
+blogPost1.printMetaData();
+
