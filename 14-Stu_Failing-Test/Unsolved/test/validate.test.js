db3dd41d1fa0a540279b67690d20147980ccd814
Josie Zobel <159351625+jobeans24@users.noreply.github.com>
2024-04-18 19:53:19 -0400

    creating the code to allow the generator to fail when testing through adding a specified password into the accepted input

--- 14-Stu_Failing-Test/Unsolved/test/validate.test.js
+++ 14-Stu_Failing-Test/Unsolved/test/validate.test.js
@@ -1,5 +1,13 @@
-// TODO: Import the Validate class.
+const Validate = require('../validate');
 
 // TODO: Describe a testing suite for checking the functionality of the Validate class.
+describe('Validate', () => {
+    describe('isPassword', () => {
+        return Validate.isPassword('password');
+    });
+});
 
 // TODO: Write a test that asserts that isPassword() returns false when passed an empty string.
+    test('should return false when passed an empty string', () => {
+        expect(Validate.isPassword('')).toBe(false);
+    });
--- 14-Stu_Failing-Test/Unsolved/validate.js
+++ 14-Stu_Failing-Test/Unsolved/validate.js
@@ -1,3 +1,15 @@
 // TODO: Define a Validate class with an isPassword() function that takes in a password parameter.
+class Validate {
+  static isPassword(password) {
+    // TODO: Implement password validation logic here
+    if (password === "GG55gsc&") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+module.exports = Validate;
 
 // TODO: Export the Validate class from this file.
